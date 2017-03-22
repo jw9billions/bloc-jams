2eff3cc36133cb15834da5cb9891682a84e2b044
JANE SWEET <jane@JANEs-Air.attlocal.net>
2017-03-21 23:31:13 -0700

    assgnment for cp10 unsuccesful

--- scripts/album.js
+++ scripts/album.js
@@ -83,10 +83,10 @@ var setCurrentAlbum = function (album) {
 };
 
 
-var findParentByClassName = function (element, targetClass){
+var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while(currentParent.className !== targetClass && currentParent.className !== null){
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
