6cf8481599bdeb1c31e33d98dd592830b202e17b
JANE SWEET <jane@JANEs-Air.attlocal.net>
2017-03-24 10:23:37 -0700

    cp15

--- scripts/album.js
+++ scripts/album.js
@@ -143,7 +143,6 @@ window.onload = function (){
     
     songListContainer.addEventListener('mouseover', function(event) {
          if (event.target.parentElement.className === 'album-view-song-item') {
-             // Change the content from the number to the play button's HTML
              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
          }
      });
